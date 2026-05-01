7399685843838cd7bf53a8ee742e9d7516171e29
iknowledges <cra452@126.com>
2026-05-01 17:32:39 +0800

    amm

---
+++ amm/programs/amm/src/instructions/mod.rs
@@ -0,0 +1,10 @@
+mod add_liquidity;
+mod init_pool;
+mod remove_liquidity;
+mod swap;
+mod common;
+
+pub use add_liquidity::*;
+pub use init_pool::*;
+pub use remove_liquidity::*;
+pub use swap::*;
---
+++ amm/programs/amm/src/lib.rs
@@ -0,0 +1,51 @@
+use anchor_lang::prelude::*;
+
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+declare_id!("AVGXNq1AXQbS2i6HFMfbWZzyDQ1Yueai8dPeZ5M3LmCy");
+
+#[program]
+pub mod amm {
+    pub use super::instructions::*;
+    use super::*;
+
+    pub fn init_pool(ctx: Context<InitPool>, fee: u16) -> Result<()> {
+        instructions::init_pool(ctx, fee)?;
+        Ok(())
+    }
+
+    pub fn add_liquidity(
+        ctx: Context<AddLiquidity>,
+        fee: u16,
+        amount_a: u64,
+        amount_b: u64,
+    ) -> Result<()> {
+        instructions::add_liquidity(ctx, fee, amount_a, amount_b)?;
+        Ok(())
+    }
+
+    pub fn remove_liquidity(
+        ctx: Context<RemoveLiquidity>,
+        fee: u16,
+        shares: u64,
+        min_amount_a: u64,
+        min_amount_b: u64,
+    ) -> Result<()> {
+        instructions::remove_liquidity(ctx, fee, shares, min_amount_a, min_amount_b)?;
+        Ok(())
+    }
+
+    pub fn swap(
+        ctx: Context<Swap>,
+        fee: u16,
+        a_for_b: bool,
+        amount_in: u64,
+        min_amount_out: u64,
+    ) -> Result<()> {
+        instructions::swap(ctx, fee, a_for_b, amount_in, min_amount_out)?;
+        Ok(())
+    }
+}
