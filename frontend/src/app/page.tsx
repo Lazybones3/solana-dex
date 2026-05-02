827c484ac91b02580b21987edf2a6867a9f69926
iknowledges <cra452@126.com>
2026-05-02 22:13:17 +0800

    frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { WalletButton } from "@/components/solana/solana-provider";
+
+export default function Home() {
+  return (
+    <div>
+      <WalletButton />
+    </div>
+  );
+}
---
+++ frontend/src/components/solana/solana-provider.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import dynamic from 'next/dynamic'
+import { ReactNode, useCallback, useMemo } from 'react'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import { clusterApiUrl } from '@solana/web3.js'
+
+export const WalletButton = dynamic(async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton, {
+  ssr: false,
+})
+
+export function SolanaProvider({ children }: { children: ReactNode }) {
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const onError = useCallback((error: WalletError) => {
+    console.error(error)
+  }, [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} onError={onError} autoConnect={true}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
