9339f8d54693277b4451167cd03b7dc07618a469
Lazybones3 <caorui452@163.com>
2026-05-11 19:59:18 +0800

    update tokenList

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,16 +1,33 @@
 export const tokenList = [
   {
-    name: "My Awesome Token",
-    symbol: "MAT",
-    mint: "J5pF63WvJPKJbug7dhLA9Xv5UEyxjsZPwXzAGCmvNmjd",
-  }, {
-    name: "Another Great Token",
-    symbol: "AGT",
-    mint: "9kzZZ4byEXDunqsRu4FWNHPAdV9ErwudoTz44g9udKBC",
-  }, {
-    name: "USDC",
-    symbol: "USDC",
-    mint: "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU",
+    name: "Omegamon",
+    symbol: "OME",
+    mint: "CrHwhJFWgEW2CnAF7XbpTEvNQiFpTYmmysJwpgQygVkm",
+  },
+  {
+    name: "Magnamon",
+    symbol: "MAG",
+    mint: "7TLyti32gxHYTKsHaZyeuMfAU9Adr9Bd72gexzZ1Q4Tw",
+  },
+  {
+    name: "Kentaurosmon",
+    symbol: "KEN",
+    mint: "2aev7xp3myQmZTq7QrR82cFAybct6LmWMZ9y9Yi2HnwQ",
+  },
+  {
+    name: "Jesmon",
+    symbol: "JES",
+    mint: "7qnyCv7FJWpSmZy6qLpMcGGJASE5EYLfbfReMZ3KK8N2",
+  },
+  {
+    name: "Gallantmon",
+    symbol: "GAL",
+    mint: "CbBHrwvCKu44qUV91gxY7VkYi1uA1NDaXcri989m1WBR",
+  },
+  {
+    name: "Examon",
+    symbol: "EXA",
+    mint: "FMDy72xp13F1xaVGXZN4hzMPm1VfAaJjbLCp5MA93YMF",
   },
 ];
 
